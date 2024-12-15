b9dda124ba7d94b7a55284e2e524abf54c98e1f6
nikhil <nikhilsharma270027@gmail.com>
2024-12-15 23:15:57 +0530

    add bookmark

--- app/(tabs)/Bookmark.tsx
+++ app/(tabs)/Bookmark.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
 import React from 'react'
+import SearchInput from '@/components/SearchInput'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+    <View className='flex my-6 px-4 space-y-6'>
+
+<View className='justify-between items-start flex-row m-6'>
+  <View className=''>
+    {/* <Text className='font-pmedium text-sm text text-gray-100'>
+        
+    </Text> */}
+    <Text className='text-2xl font-psemibold text-white'>
+      {/* {user?.username} */}
+Bookmark
+    </Text>
+  </View>
+  <View className='mt-1.5'>
+    <Image
+      source={images.logoSmall}
+      className='w-9 h-10'
+      resizeMode='contain'
+    />  
+  </View>
+</View>
+
+<SearchInput />
+
+</View>
+</SafeAreaView>
+          
   )
 }
 
